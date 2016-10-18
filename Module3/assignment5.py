a7c674403b0ba434e05dd08dd29372d612605fc4
tonytunde2012 <tonytunde2012@gmail.com>
2016-10-18 19:30:20 +0100

    Finished Module 3

--- Module3/assignment5.py
+++ Module3/assignment5.py
@@ -1,4 +1,33 @@
+# %load assignment5.py
 #
 # This code is intentionally missing!
 # Read the directions on the course lab page!
 #
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+
+#import andrews_curves
+from pandas.tools.plotting import andrews_curves
+
+#Read in the dataset
+wheatAndrew = pd.read_csv('Datasets/wheat.data')
+
+#Drop 'id', 'area' and 'perimeter'
+wheatADrop = wheatAndrew.drop(['id', 'area', 'perimeter'], axis = 1)
+wheatADrop.head()
+
+#Prepare Andrew plot for the data
+plt.figure()
+andrews_curves(wheatADrop, 'wheat_type')
+plt.show()
+
+
+#Add back 'area' and 'perimeter'
+wheatComp = pd.concat([wheatADrop, wheatAndrew.area,  wheatAndrew.perimeter], axis = 1)
+
+#Plot Andrew Curve
+plt.figure()
+andrews_curves(wheatComp, 'wheat_type')
+plt.show()
+#With the Addition of the other features 'area' and 'perimeter' the feature scaling is no more feasible
--- Module3/assignment6.py
+++ Module3/assignment6.py
@@ -1,32 +1,37 @@
+# %load assignment6.py
 import pandas as pd
 import matplotlib.pyplot as plt
-
 
 #
 # TODO: Load up the Seeds Dataset into a Dataframe
 # It's located at 'Datasets/wheat.data'
 # 
 # .. your code here ..
-
+wheatCorr = pd.read_csv('Datasets/wheat.data')
 
 #
 # TODO: Drop the 'id' feature
 # 
 # .. your code here ..
-
+wheatCorrDrop = wheatCorr.drop('id', axis = 1)
 
 #
 # TODO: Compute the correlation matrix of your dataframe
 # 
 # .. your code here ..
-
+print(wheatCorrDrop.corr())
 
 #
 # TODO: Graph the correlation matrix using imshow or matshow
 # 
 # .. your code here ..
 
+plt.imshow(wheatCorrDrop.corr(), cmap = plt.cm.Greens, interpolation= 'nearest')
+plt.colorbar()
+tick_marks = [i for i in range(len(wheatCorrDrop.columns))]
+plt.xticks(tick_marks, wheatCorrDrop.columns, rotation = 'vertical')
+plt.yticks(tick_marks, wheatCorrDrop.columns)
+
 
 plt.show()
-
 
