353504e2785d17082f15074dc08eca30123f07e8
pcurzytek <curzytekp@gmail.com>
2017-06-17 13:44:40 -0400

    labs2 solution

--- Module2/assignment3.py
+++ Module2/assignment3.py
@@ -3,14 +3,17 @@ import pandas as pd
 # TODO: Load up the dataset
 # Ensuring you set the appropriate header column names
 #
-# .. your code here ..
+names=['motor', 'screw', 'pgain', 'vgain', 'class']
+df = pd.read_csv('Datasets/servo.data', names=names)
+print df
 
 
 # TODO: Create a slice that contains all entries
 # having a vgain equal to 5. Then print the 
 # length of (# of samples in) that slice:
 #
-# .. your code here ..
+df_vgain = df[df.vgain == 5]
+print len(df_vgain)
 
 
 # TODO: Create a slice that contains all entries
@@ -18,8 +21,8 @@ import pandas as pd
 # to E. Then print the length of (# of
 # samples in) that slice:
 #
-# .. your code here ..
-
+df_motor_and_screw = df[(df.motor == 'E') & (df.screw == 'E')]
+print len(df_motor_and_screw)
 
 
 # TODO: Create a slice that contains all entries
@@ -28,12 +31,13 @@ import pandas as pd
 # value for the samples in that slice. Once
 # you've found it, print it:
 #
-# .. your code here ..
+df_pgain = df[df.pgain == 4]
+print df_pgain.describe().vgain[1]
 
 
 
 # TODO: (Bonus) See what happens when you run
 # the .dtypes method on your dataframe!
-
+print df.dtypes
 
 
