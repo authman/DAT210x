e0ead40a0fd9d0cf6f72bb413064bd93b06ffc2e
pcurzytek <curzytekp@gmail.com>
2017-06-19 23:04:52 -0400

    labs4 solution

--- Module4/assignment4.py
+++ Module4/assignment4.py
@@ -6,6 +6,9 @@ import random, math
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
+from sklearn.decomposition import PCA
+from sklearn import manifold
+
 def Plot2D(T, title, x, y, num_to_plot=40):
   # This method picks a bunch of random samples (images in your case)
   # to plot onto the chart:
@@ -27,6 +30,18 @@ def Plot2D(T, title, x, y, num_to_plot=40):
   ax.scatter(T[:,x],T[:,y], marker='.',alpha=0.7)
 
 
+def Plot3D(T, title, x, y, z, num_to_plot=40):
+  # This method picks a bunch of random samples (images in your case)
+  # to plot onto the chart:
+  fig = plt.figure()
+  ax = fig.add_subplot(111, projection='3d')
+  ax.set_title(title)
+  ax.set_xlabel('Component: {0}'.format(x))
+  ax.set_ylabel('Component: {0}'.format(y))
+  ax.set_zlabel('Component: {0}'.format(z))
+  
+  # It also plots the full scatter:
+  ax.scatter(T[:,x],T[:,y], T[:,z], marker='.',alpha=0.7)
 
 # A .MAT file is a .MATLAB file. The faces dataset could have came
 # in through .png images, but we'll show you how to do that in
@@ -40,33 +55,30 @@ num_pixels = int(math.sqrt(num_pixels))
 for i in range(num_images):
   df.loc[i,:] = df.loc[i,:].reshape(num_pixels, num_pixels).T.reshape(-1)
 
-
-#
 # TODO: Implement PCA here. Reduce the dataframe df down
 # to THREE components. Once you've done that, call Plot2D.
-#
-# The format is: Plot2D(T, title, x, y, num_to_plot=40):
-# T is your transformed data, NDArray.
-# title is your chart title
-# x is the principal component you want displayed on the x-axis, Can be 0 or 1
-# y is the principal component you want displayed on the y-axis, Can be 1 or 2
-#
-# .. your code here ..
+pca = PCA(n_components=3, svd_solver='full')
+pca.fit(df)
+T_pca = pca.transform(df)
+Plot2D(T_pca, 'PCA_0_1', 0, 1)
+Plot2D(T_pca, 'PCA_1_2', 1, 2)
+Plot3D(T_pca, 'PCA_3D', 0, 1, 2)
 
+def IsoPlot(neighbors=5): 
+    iso = manifold.Isomap(n_neighbors=neighbors, n_components=3)
+    iso.fit(df)
+    T_iso = iso.transform(df)
+    Plot2D(T_iso, 'ISO_0_1', 0, 1)
+    Plot2D(T_iso, 'ISO_1_2', 1, 2)
+    Plot3D(T_iso, 'ISO_3D', 0, 1, 2)
 
-#
-# TODO: Implement Isomap here. Reduce the dataframe df down
-# to THREE components. Once you've done that, call Plot2D using
-# the first two components.
-#
-# .. your code here ..
-
+for i in range(3,4):
+    IsoPlot(i)
 
 #
 # TODO: If you're up for a challenge, draw your dataframes in 3D
 # Even if you're not, just do it anyway.
 #
-# .. your code here ..
 
 
 plt.show()
--- Module4/assignment5.py
+++ Module4/assignment5.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 
 from scipy import misc
@@ -5,18 +6,14 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib
 import matplotlib.pyplot as plt
 
-# Look pretty...
-# matplotlib.style.use('ggplot')
+from sklearn import manifold
+
 plt.style.use('ggplot')
 
-
-#
 # TODO: Start by creating a regular old, plain, "vanilla"
 # python list. You can call it 'samples'.
-#
-# .. your code here .. 
-
-#
+samples = []
+colors = []
 # TODO: Write a for-loop that iterates over the images in the
 # Module4/Datasets/ALOI/32/ folder, appending each of them to
 # your list. Each .PNG image should first be loaded into a
@@ -28,51 +25,59 @@ plt.style.use('ggplot')
 # 0-255  to  0.0-1.0  if you'd like, but that will have no
 # effect on the algorithm's results.
 #
-# .. your code here .. 
+def append_images(samples, path, colors, color):     
+    for root, dirs, files in os.walk(path):
+        for fname in files:
+            img = misc.imread(path + fname)
+            samples.append(img[::2,::2].reshape(-1))
+            colors.append(color)
 
+append_images(samples, 'Datasets/ALOI/32/', colors, 'b')
 
-#
 # TODO: Once you're done answering the first three questions,
 # right before you converted your list to a dataframe, add in
 # additional code which also appends to your list the images
 # in the Module4/Datasets/ALOI/32_i directory. Re-run your
 # assignment and answer the final question below.
 #
-# .. your code here .. 
+append_images(samples, 'Datasets/ALOI/32i/', colors, 'r')
 
+df = pd.DataFrame(samples)
 
-#
-# TODO: Convert the list to a dataframe
-#
-# .. your code here .. 
-
-
-
-#
 # TODO: Implement Isomap here. Reduce the dataframe df down
 # to three components, using K=6 for your neighborhood size
-#
-# .. your code here .. 
-
-
+iso = manifold.Isomap(n_neighbors=6, n_components=3)
+iso.fit(df)
+T = iso.transform(df)
 
 #
 # TODO: Create a 2D Scatter plot to graph your manifold. You
 # can use either 'o' or '.' as your marker. Graph the first two
 # isomap components
 #
-# .. your code here .. 
-
-
-
+def plot_2D(T, colors, x=0, y=1):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_title('ISO 2D scatter plot')
+    ax.set_xlabel('C: {0}'.format(x))
+    ax.set_ylabel('C: {0}'.format(y))
+    ax.scatter(T[:,x], T[:,y], marker='o', alpha=1.0, c=colors)
 
 #
 # TODO: Create a 3D Scatter plot to graph your manifold. You
 # can use either 'o' or '.' as your marker:
 #
-# .. your code here .. 
+def plot_3D(T, colors, x=0, y=1, z=2):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_title('ISO 3D scatter plot')
+    ax.set_xlabel('C: {0}'.format(x))
+    ax.set_ylabel('C: {0}'.format(y))
+    ax.set_zlabel('C: {0}'.format(z))
+    ax.scatter(T[:,x], T[:,y], T[:,z], marker='o', alpha=1.0, c=colors)
 
 
-
+plot_2D(T, colors)
+plot_3D(T, colors)
 plt.show()
 
